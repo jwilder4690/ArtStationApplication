d1cd61c37934ad33551ae53f66ab59bd01792981
jwilder4690 <thatoneguy4690@gmail.com>
2018-11-20 07:00:44 -0500

    Merged More features
    - Fixed scaling issue by having the java application start at the
    display width and display height, and the BorderPane scaled accordingly.
    
    - Ported over mousePressed, circle, and drawing to canvas functionality

--- src/artstationapplication/ArtStationApplication.java
+++ src/artstationapplication/ArtStationApplication.java
@@ -15,6 +15,7 @@ import javafx.stage.Stage;
 import processing.core.*;
 import processing.javafx.PSurfaceFX;
 import javafx.scene.image.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -28,10 +29,9 @@ public class ArtStationApplication extends PApplet{
     Mode activeMode = Mode.DRAW;
     CanvasArea pad = new CanvasArea(this,900,900);
     float scaleFactor;
-    int toolbarWidth = 200;
     float verticalPadding = 0.05f;
     float verticalScreenShare = 1 - 2*verticalPadding;
-    float horizontalPadding = 0.15f;
+    float horizontalPadding = 0.05f;
     float horizontalScreenShare = 1 - 2*horizontalPadding;
     boolean[] keys = new boolean[255];
     boolean controlKey = false;
@@ -43,7 +43,8 @@ public class ArtStationApplication extends PApplet{
 
     @Override
     public void settings(){
-        size(1280,720, FX2D);
+        size(displayWidth,displayHeight, FX2D);
+        //size(1280,720, FX2D);
     }
     
     @Override
@@ -63,7 +64,7 @@ public class ArtStationApplication extends PApplet{
         Image imageCircle = new Image(getClass().getResource("data/btnCircle.png").toExternalForm());
         ToggleButton btnCircle = new ToggleButton("Circle", new ImageView(imageCircle));
         btnCircle.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-        btnCircle.setStyle("-fx-padding:0");
+        btnCircle.setStyle("-fx-padding:0; ");
         btnCircle.setToggleGroup(tg);
         
         Image imageSquare = new Image(getClass().getResource("data/btnSquare.png").toExternalForm());
@@ -89,6 +90,7 @@ public class ArtStationApplication extends PApplet{
         final TilePane toolPane = new TilePane(2,2);
         toolPane.setBackground(new Background(new BackgroundFill(Color.CADETBLUE, new CornerRadii(0),new Insets(0))));
         toolPane.getChildren().addAll(btnCircle, btnSquare, btnTriangle, btnLine);
+        toolPane.setPrefColumns(1);
         //rootNode.setTop(mb);
         rootNode.setLeft(toolPane);
         rootNode.setCenter(canvas);
@@ -97,13 +99,11 @@ public class ArtStationApplication extends PApplet{
         stage.widthProperty().addListener((obs, oldVal, newVal) -> {
             // Do whatever you want
             scaleCanvas((float)stage.getWidth(),(float)stage.getHeight());
-            System.out.println("Width Changed. New scaleFactor: "+scaleFactor);   
         });
 
         stage.heightProperty().addListener((obs, oldVal, newVal) -> {
              // Do whatever you want
              scaleCanvas((float)stage.getWidth(),(float)stage.getHeight());
-             System.out.println("Height Changed. New scaleFactor: "+scaleFactor);    
         });
         
         Platform.runLater(new Runnable() {
@@ -120,14 +120,34 @@ public class ArtStationApplication extends PApplet{
         //checkInput();
         background(55,55,55);
         drawCanvasArea();
-        //drawFrames();
-        //drawToolbar();
+        drawFrames();
     }
     
+    @Override
+    public void mousePressed(){
+        if(activeMode == Mode.DRAW){
+          if(canvasX >= 0 && canvasY >= 0 && canvasX <= pad.getHeight() && canvasY <= pad.getWidth()){
+            pad.drawShape(canvasX, canvasY, activeTool);
+          }
+        }
+        else if(activeMode == Mode.EDIT){
+          //???
+        }
+      }
+    
+    void drawFrames(){
+        rectMode(CORNER);
+        noStroke();
+        fill(55,55,55);
+        rect(0,0, width*horizontalPadding, height);
+        rect(0,0, width, height*verticalPadding);
+        rect(0, height*verticalPadding + (pad.getHeight()*scaleFactor), width, height*verticalPadding);
+        rect( width*horizontalPadding + (pad.getWidth()*scaleFactor), 0, width*horizontalPadding, height);
+      }
+    
     void drawCanvasArea(){
-        //scaleCanvas();
         pushMatrix();
-          translate(width*horizontalPadding +toolbarWidth, height*verticalPadding);
+          translate(width*horizontalPadding, height*verticalPadding);
           scale(scaleFactor,scaleFactor);
           pad.drawCanvas(canvasX, canvasY);
           canvasX = (mouseX - screenX(0,0))/scaleFactor;
@@ -149,7 +169,7 @@ public class ArtStationApplication extends PApplet{
     }
     
      class CanvasArea {
-         private PApplet sketch;
+        private PApplet sketch;
         final int CENT = 0;
         final int CORN = 4;
         int canvasWidth;
@@ -158,7 +178,7 @@ public class ArtStationApplication extends PApplet{
         int gridDensity = 10; //must be less than canvasWidth if int division is used
         float gridSpacing;
         boolean gridOn = true;
-        //ArrayList<Shape> shapes = new ArrayList<Shape>();
+        ArrayList<Shape> shapes = new ArrayList<>();
         int numberOfShapes = 0;
         int currentShapeIndex = 0;
 
@@ -182,47 +202,47 @@ public class ArtStationApplication extends PApplet{
             strokeWeight(0);
             stroke(0, 0, 0);
             rect(0, 0, canvasWidth, canvasHeight);
-//            if (mousePressed) {
-//                update(mx, my);
-//            }
-//            for (int i = 0; i < shapes.size(); i++) {
-//                shapes.get(i).drawShape();
-//            }
+            if (mousePressed) {
+                update(mx, my);
+            }
+            for (int i = 0; i < shapes.size(); i++) {
+                shapes.get(i).drawShape();
+            }
             if (gridOn) {
                 drawGrid();
             }
         }
 
-//        void update(float x, float y) {
-//            switch (activeMode) {
-//                case DRAW:
-//                    shapes.get(shapes.size() - 1).modify(x, y);
-//                    break;
-//                case EDIT:
-//                    shapes.get(currentShapeIndex).manipulate(x, y);
-//                    break;
-//            }
-//        }
+        void update(float x, float y) {
+            switch (activeMode) {
+                case DRAW:
+                    shapes.get(shapes.size() - 1).modify(x, y);
+                    break;
+                case EDIT:
+                    shapes.get(currentShapeIndex).manipulate(x, y);
+                    break;
+            }
+        }
 
-//        void drawShape(float x, float y, ShapeType type) {
-//            switch (type) {
-//                default:
-//                    numberOfShapes++;
-//                    currentShapeIndex = numberOfShapes - 1;
-//                case CIR:
-//                    shapes.add(new Circle(x, y, 50));
-//                    break;
-//                case REC:
-//                    shapes.add(new Rectangle(x, y, 50, CORN));
-//                    break;
-//                case TRI:
-//                    shapes.add(new Triangle(x, y, 50));
-//                    break;
-//                case LIN:
-//                    shapes.add(new Line(x, y, 50, 50));
-//                    break;
-//            }
-//        }
+        void drawShape(float x, float y, ShapeType type) {
+            switch (type) {
+                default:
+                    numberOfShapes++;
+                    currentShapeIndex = numberOfShapes - 1;
+                case CIR:
+                    shapes.add(new Circle(x, y, 50));
+                    break;
+                case REC:
+                    //shapes.add(new Rectangle(x, y, 50, CORN));
+                    break;
+                case TRI:
+                    //shapes.add(new Triangle(x, y, 50));
+                    break;
+                case LIN:
+                    //shapes.add(new Line(x, y, 50, 50));
+                    break;
+            }
+        }
 
 //        void toggleSelectShape(boolean toggleOn) {
 //            if (toggleOn) {
@@ -249,5 +269,75 @@ public class ArtStationApplication extends PApplet{
             }
         }
     }
+     
+     abstract class Shape{
+        private PApplet sketch;
+        PVector pos;
+        float rotation = 0;
+        int paint = Integer.parseInt("00FF00", 16);
+        float lineThickness = 2;
+        boolean finished = false;
+        boolean selected = false;
+  
+        Shape(float x, float y){
+          pos = new PVector(x,y);
+        }
 
+        public void initSketch(PApplet parent){
+            sketch = parent;
+        }
+        
+        abstract void drawShape();
+
+        abstract void modify(float x, float y);
+
+        void manipulate(float x, float y){
+          pos.set(x,y);
+        }
+
+        void finishShape(){
+          finished = true;
+        }
+
+        void select(){
+          selected = true;
+        }   
+
+        void deselect(){
+          selected = false;
+        }
+     }
+     
+     class Circle extends Shape{
+        float radius;
+        float widthModifier = 1;
+        float heightModifier = 1;
+
+        Circle(float x, float y, float r){
+          super(x,y);
+          radius = r;   
+        }
+
+        @Override
+        void drawShape(){
+          fill(paint);
+          strokeWeight(lineThickness);
+          pushMatrix();
+          translate(pos.x, pos.y);
+          ellipse(0,0, radius*widthModifier, radius*heightModifier);
+          if(selected){
+            noFill();
+            strokeWeight(3);
+            stroke(255,255, 0);
+            ellipse(0,0, radius*widthModifier, radius*heightModifier);
+          }
+          popMatrix();
+        }
+
+        @Override
+        void modify(float x, float y){
+          radius = 2*dist(x,y, pos.x, pos.y);
+        }
+
+      }
 }
