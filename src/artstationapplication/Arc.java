4aa6c311ffd24cd147e6cbae6312bc9482ab512e
jwilder4690 <thatoneguy4690@gmail.com>
2019-03-05 06:30:44 -0500

    Fleshing out Arc Feature
    - Added handles for start and end.
    - Updated drawing method.
    -Added helper method to find angle of start and end handles.

--- src/artstationapplication/Arc.java
+++ src/artstationapplication/Arc.java
@@ -5,10 +5,275 @@
  */
 package artstationapplication;
 
+import processing.core.*;
+
 /**
  *
  * @author wilder4690
  */
-public class Arc {
+class Arc extends Shape{
+    Handle widthHandleR;
+    Handle widthHandleL;
+    Handle heightHandleT;
+    Handle heightHandleB;
+    VertexHandle start;
+    VertexHandle end;
+    Handle[] activeHandle = new Handle[2];
+    Handle[] inactiveHandle = new Handle[2];
+
+    Arc(PApplet drawingSpace, int paint, int outline, float thickness, float x, float y, int id){
+      super(drawingSpace,paint,outline,x,y);
+      strokeWeight = thickness;
+      index = id;
+      widthHandleR = new Handle(drawingSpace, this, new PVector(1,0));
+      widthHandleL = new Handle(drawingSpace, this, new PVector(-1,0));
+      heightHandleB = new Handle(drawingSpace, this, new PVector(0,1));
+      heightHandleT = new Handle(drawingSpace, this, new PVector(0,-1));
+      start = new VertexHandle(drawingSpace, new PVector(0,-1));
+      end = new VertexHandle(drawingSpace, new PVector(1,0));
+      name = "Arc";
+    }
+        
+    /*
+      Copy Constructor
+      Used for creating an exact copy of base shape.
+    */
+    Arc(Arc base, int id){
+      this(base.app, base.fillColor, base.strokeColor, base.strokeWeight, base.pos.x+base.COPY_OFFSET, base.pos.y+base.COPY_OFFSET, id);
+      widthHandleR = new Handle(base.widthHandleR, this);
+      widthHandleL = new Handle (base.widthHandleL, this);
+      heightHandleB = new Handle (base.heightHandleB, this);
+      heightHandleT = new Handle (base.heightHandleT, this);
+      start = new VertexHandle(base.app, base.start.getPosition());
+      end = new VertexHandle(base.app, base.end.getPosition());
+      rotation = base.rotation;
+      this.name = base.name;
+    }
     
-}
+    /*
+      Load Constructor
+      Used for creating shape from information stored in save file.
+    */ 
+    Arc(PApplet drawingSpace, String[] input){
+        this(drawingSpace, Integer.valueOf(input[0]), Integer.valueOf(input[1]), Float.valueOf(input[6]), Float.valueOf(input[2]), Float.valueOf(input[3]), Integer.valueOf(input[7]));
+        startingRotation = Float.valueOf(input[4]);
+        rotation = Float.valueOf(input[5]);
+        widthHandleR = new Handle(drawingSpace, this, input[8].split("&"));
+        widthHandleL = new Handle(drawingSpace, this, input[9].split("&"));
+        heightHandleT = new Handle(drawingSpace, this, input[10].split("&"));
+        heightHandleB = new Handle(drawingSpace, this, input[11].split("&"));
+        start = new VertexHandle(drawingSpace, input[12].split("&"));
+        end = new VertexHandle(drawingSpace, input[13].split("&"));
+        name = input[12];
+    }
+
+    @Override 
+    boolean mouseOver(PVector mouse){
+        //This equation checks for point inside rotated ellipse. No changes needed to be made to account for y down coordinate system. 
+        if(app.sq(app.cos(rotation)*(mouse.x - pos.x)+ app.sin(rotation)*(mouse.y - pos.y))/app.sq(widthHandleR.getRadius()) + app.sq(app.sin(rotation)*(mouse.x - pos.x)- app.cos(rotation)*(mouse.y - pos.y))/app.sq(heightHandleT.getRadius()) >= 1) return false;
+        else return true;
+    }
+
+    @Override
+    void drawShape(){
+      if(fillColor == NONE){
+          app.noFill();
+      }
+      else{
+        app.fill(fillColor);
+      }
+      if(strokeWeight == 0){
+        app.noStroke();
+      }
+      else{
+        app.stroke(strokeColor);
+        app.strokeWeight(strokeWeight);
+      }
+      app.pushMatrix();
+      app.translate(pos.x, pos.y);
+      app.rotate(rotation);
+      app.arc(0,0, 2*widthHandleL.getRadius(), 2*heightHandleT.getRadius(), getAngle(start.getPosition()), getAngle(end.getPosition()));
+      app.popMatrix();
+    }
+    
+    @Override
+    void drawSelected(){
+        app.pushMatrix();
+        app.translate(pos.x, pos.y);
+        app.rotate(rotation);
+        app.noFill();
+        app.strokeWeight(3);
+        app.stroke(editColor);
+        app.arc(0,0, 2*widthHandleL.getRadius(), 2*heightHandleT.getRadius(), getAngle(start.getPosition()), getAngle(end.getPosition()));
+        drawHandles();
+        app.popMatrix();
+    }
+    
+    /*
+        Helper method to find the angle between a handle and the x-axis
+    */
+    float getAngle(PVector vec){
+        return PVector.angleBetween(vec, new PVector(1,0));
+    }
+
+    void drawHandles(){   
+        widthHandleL.drawHandle();
+        widthHandleR.drawHandle();
+        heightHandleT.drawHandle();
+        heightHandleB.drawHandle();      
+        start.drawHandle();
+        end.drawHandle();
+    }
+
+    @Override
+    /*
+      Uses mouse position relative to shape position to scale shape.
+      Used only during initial drawing to canvas.
+    */
+    void modify(PVector mouse){
+      float radius = app.dist(mouse.x, mouse.y, pos.x, pos.y);
+      rotation = 0; //no rotation when initially drawing
+      widthHandleR.calculateModifier(radius);
+      widthHandleL.calculateModifier(radius);
+      heightHandleT.calculateModifier(radius);
+      heightHandleB.calculateModifier(radius);
+    }
+    
+
+        @Override
+    void resizeHandles(float size){
+        widthHandleL.scaleSize(size);
+        widthHandleR.scaleSize(size);
+        heightHandleT.scaleSize(size);
+        heightHandleB.scaleSize(size);
+    }
+    
+    @Override
+    boolean checkHandles(PVector mouse){
+        if(widthHandleL.overHandle(mouse, rotation) || widthHandleR.overHandle(mouse,rotation)){
+            activeHandle[0] = widthHandleL;
+            activeHandle[1] = widthHandleR;
+            inactiveHandle[0] = heightHandleT;
+            inactiveHandle[1] = heightHandleB;
+            return true;
+        }
+        else if (heightHandleT.overHandle(mouse,rotation) || heightHandleB.overHandle(mouse,rotation)){
+            activeHandle[0] = heightHandleT;
+            activeHandle[1] = heightHandleB;
+            inactiveHandle[0] = widthHandleL;
+            inactiveHandle[1] = widthHandleR;
+            return true;
+        }
+        else return false;
+    }    
+    
+    @Override
+    void adjustActiveHandle(PVector mouse){
+        float ratio = inactiveHandle[0].getRadius()/activeHandle[0].getRadius();
+        float dist = app.dist(pos.x, pos.y, mouse.x, mouse.y);
+        if(shift){  
+            //If shift is held, inactive handles are scaled proportionally with the active controller.
+            //First calulates ratio between current handles, then scales handles accordingly
+            
+            activeHandle[0].calculateModifier(dist); 
+            activeHandle[1].calculateModifier(dist);
+            inactiveHandle[0].calculateModifier(dist * ratio);  
+            inactiveHandle[1].calculateModifier(dist * ratio);
+        }
+        else{
+            activeHandle[0].calculateModifier(dist);  
+            activeHandle[1].calculateModifier(dist);
+        }
+    }
+    
+    @Override
+    void finishShape(){
+        super.finishShape();
+        widthHandleL.setRadius();
+        widthHandleR.setRadius();
+        heightHandleT.setRadius();
+        heightHandleB.setRadius();
+    }
+    
+    @Override
+    float[] getResetFloats(){
+        return new float[] {widthHandleL.getModifier(), widthHandleR.getModifier(), heightHandleT.getModifier(), heightHandleB.getModifier(), rotation};
+    }
+    
+    @Override
+    void setHandles(float[] mods){
+        widthHandleL.setModifier(mods[0]);
+        widthHandleR.setModifier(mods[1]);
+        heightHandleT.setModifier(mods[2]);
+        heightHandleB.setModifier(mods[3]);
+    }
+    
+    @Override
+    void reset(){
+        rotation = 0;
+        widthHandleL.reset();
+        widthHandleR.reset();
+        heightHandleT.reset();
+        heightHandleB.reset();
+    }
+    
+    Shape copy(int id){
+        Arc copy = new Arc(this, id);
+        return copy;
+    }
+    
+    @Override
+    String printToClipboard(){
+        String output = "";
+        if(fillColor == NONE) output += "\tnoFill();\n";
+        else output += "\tfill("+fillColor+");\n";
+
+        if(strokeWeight == 0) output += "\tnoStroke();\n";
+        else output += "\tstrokeWeight("+strokeWeight+");\n\tstroke("+strokeColor+");\n";
+
+        output += "\tpushMatrix();\n";
+        output += "\ttranslate("+pos.x+", "+pos.y+");\n";
+        output += "\trotate("+rotation+");\n";
+        output += "\tellipse(0,0,"+2*widthHandleL.getRadius()+", "+2*heightHandleT.getRadius()+");\n";
+        output += "\tpopMatrix();\n\n";
+        
+        return output;
+    }
+    
+    @Override
+    PGraphics printToPGraphic(PGraphics ig){
+      if(fillColor == NONE){
+          ig.noFill();
+      }
+      else{
+        ig.fill(fillColor);
+      }
+      if(strokeWeight == 0){
+        ig.noStroke();
+      }
+      else{
+        ig.stroke(strokeColor);
+        ig.strokeWeight(strokeWeight);
+      }
+      ig.pushMatrix();
+      ig.translate(pos.x, pos.y);
+      ig.rotate(rotation);
+      ig.ellipse(0,0, 2*widthHandleL.getRadius(), 2*heightHandleT.getRadius());
+      ig.popMatrix();
+      return ig;
+    }
+    
+    @Override
+    String save(){
+        String output ="Arc;";
+        output += fillColor+","+strokeColor+","+pos.x+","+pos.y+","+startingRotation+","+rotation+","+strokeWeight+","+index+",";
+        output += widthHandleL.save()+",";
+        output += widthHandleR.save()+",";
+        output += heightHandleT.save()+",";
+        output += heightHandleB.save()+",";
+        output += start.save()+",";
+        output += end.save()+",";
+        output += this.name;
+        return output;
+    }
+ }
